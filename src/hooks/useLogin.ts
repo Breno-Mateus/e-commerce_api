d3b1d3f9fc65a2118c0eae874d2eadbcfe27ff4a
Breno-Mateus <brenomateus36@gmail.com>
2025-04-25 08:46:18 -0300

    feat: update useLogin hook

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,12 +1,15 @@
 import { useMutation } from "@tanstack/react-query";
 import { loginUser } from "@/services/authService";
 import { toast } from "sonner";
+import { useAuthStore } from "@/store/useAuthStore";
 
 export const useLogin = () => {
+  const setToken = useAuthStore((state) => state.setToken);
+
   return useMutation({
     mutationFn: loginUser,
     onSuccess: (data) => {
-      localStorage.setItem("token", data.token);
+      setToken(data.token);
       toast.success("Login bem-sucedido!")
       console.log("Login bem-sucedido!", data.token);
     },
