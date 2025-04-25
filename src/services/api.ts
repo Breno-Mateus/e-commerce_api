8a105d3ed87f38bd6b897b1abc646d62680bf18c
Breno-Mateus <brenomateus36@gmail.com>
2025-04-25 08:48:53 -0300

    feat: add interceptors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,18 @@
 import axios from "axios";
+import { useAuthStore } from "@/store/useAuthStore";
 
-export const api = axios.create({
+const api = axios.create({
   baseURL: "https://fakestoreapi.com",
 });
+
+api.interceptors.request.use((config) => {
+  const token = useAuthStore.getState().token;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export default api;
